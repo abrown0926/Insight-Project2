dea3e02ce9a39504b8b1ea5f4e010a59036ceef1
Geneva Diaz <kukla1422@gmail.com>
2021-11-01 18:43:07 -0500

    changes to server.js file, package.json, and seeds.js to make boilerplate

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const path = require('path');
-const express = require('express');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
-const sequelize = require('./config/connection');
+// boilerplate
+
+const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+const helpers = require("./utils/helpers");
+const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,16 +13,29 @@ const PORT = process.env.PORT || 3001;
 // Create the Handlebars.js engine object with custom helper functions
 const hbs = exphbs.create({ helpers });
 
+// TODO: Add a comment describing the functionality of this object
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+// TODO: Add a comment describing the functionality of this statement
+app.use(session(sess));
 // Inform Express.js which template engine we're using
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
